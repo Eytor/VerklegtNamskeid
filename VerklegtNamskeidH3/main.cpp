66a4612d7f3a4f0e31c88305ce8fdcd22b633d8e
Eyþór Atli Sigurðsson <Eyr Atli Sigursson>
2016-11-28 14:54:13 +0000

    Fixed display, yo mama is a snowblower

--- VerklegtNamskeidH3/main.cpp
+++ VerklegtNamskeidH3/main.cpp
@@ -18,23 +18,26 @@ void updateFile(vector<TolPers> person, int num, int& count)
 
     for(int i = count; i < (count+num); i++)
     {
-        file >> person[i].name >> person[i].sex >> person[i].yearOfBirth >> person[i].yearOfDeath ;
+        file >> person[i].name ;
+        file >> person[i].sex ;
+        file >> person[i].yearOfBirth ;
+        file >> person[i].yearOfDeath ;
     }
     file.close();
     count += num;
 }
 
-void retriveInfo(vector<TolPers> person, int& count)
+void retriveInfo(vector<TolPers>& person, int& count)
 {
-    fstream file;
+    ifstream file;
     file.open ("list.txt");
     string name;
     string sex;
     int birth;
     int death;
+    TolPers pers;
     while(file >> name >> sex >> birth >> death)
     {
-        TolPers pers;
         pers.name = name;
         pers.sex = sex;
         pers.yearOfBirth = birth;
@@ -51,21 +54,22 @@ int selection()
     cout << "1. Input people." << endl
          << "2. Display List." << endl
          << "3. Search from list" << endl
-         << "4. Order list." << endl;
+         << "4. Order list." << endl
+         << "0. Quit." << endl;
     cin >> selectNum;
     return selectNum;
 
 }
 
-void InputPeople(vector<TolPers> person, int& count)
+void InputPeople(vector<TolPers>& person, int& count)
 {
     int numOfPeople;
     cout << "Select number or people:";
     cin >> numOfPeople;
-
+    TolPers pers;
     for(int i = 0; i < numOfPeople; i++)
     {
-        TolPers pers;
+
         cout << "Name: ";
         cin >> pers.name;
         cout << "Sex: ";
@@ -90,6 +94,7 @@ void displayList(vector<TolPers> person, int count)
                 cout << "Year of Death: " << person[i].yearOfDeath << endl;
             }
     }
+    cout << endl;
 }
 
 
@@ -100,14 +105,17 @@ int main()
     int count = 0;
     int selected;
     retriveInfo(personur, count);
+
     while(selected != 0){
         selected = selection();
         switch (selected) {
         case 1:
             InputPeople(personur, count);
+            cout << endl;
             break;
         case 2:
             displayList(personur, count);
+            cout << endl;
         break;
         case 3:
             break;
@@ -116,7 +124,7 @@ int main()
         case 0:
             break;
         default:
-            cout << "Wrong input";
+            cout << "Wrong input" << endl;
             break;
         }
     }
