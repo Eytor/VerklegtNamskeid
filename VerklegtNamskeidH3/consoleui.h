c9e60794ab39e6740c006a31e9966d9a3f37dbbe
Alexander <allimon315@gmail.com>
2016-12-02 14:29:58 +0000

    Added comments for consoleui

--- VerklegtNamskeidH3/consoleui.h
+++ VerklegtNamskeidH3/consoleui.h
@@ -7,18 +7,28 @@ class ConsoleUI
 {
 public:
     ConsoleUI();
+    //Runs the main program, where the user can select what he can do
     void run();
+    //Outputs the sort menu and lets the user pick what way to sort the list
     void sortSelector();
+    //Prints out information about one person
     void printPerson(int list, int i);
+    //Displays a list of all the person
     void displayList(int list);
+    //Takes in input from user and searches for it
     void search();
+    //Outputs the header for displayList()
     void giveHead();
-    int editSelect();
+    //Outputs a list of all people and gives the user an option to edit one of them
     void edit();
+    //Gives the user an option to add one or more people to the list
     void addToList();
+    //Sends a person to the recycle bin
     void deletePerson();
-    void displayTrash();
+    /*Gives the user options to display the recycle bin,
+    recover a person from the recycle or empty the recycle bin*/
     void trashSelector();
+    //Recovers a person from the recycle bin
     void recoverFromTrash();
 private:
     DomainService _service;
