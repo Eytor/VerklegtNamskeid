af745f9b6ffc471870469211a874f68446d71555
Alexander <allimon315@gmail.com>
2016-12-02 21:19:39 +0000

    Finished commenting code

--- VerklegtNamskeidH3/domainservice.h
+++ VerklegtNamskeidH3/domainservice.h
@@ -11,28 +11,46 @@ class DomainService
 {
 public:
     DomainService();
+    //Function that retrives the list
     void retriveList();
+    //Function that updates the list.txt file
     void updateFile();
+    //Function that updates the bin.txt file
     void updateTrash();
-    void addToList();
+    //Function that takes first name, middle initial and last name
+    //and combines them into one string and returns it
     string getName(int list, int i);
+    //Gets the first name and returns it
     string getFirstName(int i);
+    //Gets the middle initial and returns it
     string getMiddleInitial(int i);
+    //Gets the last name and returns it
     string getLastName(int i);
+    //Gets the gender and returns it
     string getGender(int list, int i);
+    //Gets the year of birth
     int getYoB(int list, int i);
+    //Gets the year of death
     int getYoD(int list, int i);
+    //Gets the size of the list
     int getListSize(int list);
+    //Checks if the list is empty
     bool getEmptyStatus(int list);
-    void printPerson(int list, int i);
-    void displayList();
+    //Converts a string to lower
     string convertToLower(string unfilteredString);
+    //Function that recives a string and searches for it on the list
     vector<int> search(string keyword);
+    //Function that sorts the list in different ways
     void whatToSort(int selector);
+    //Function that edits a value of a person
     void edit(int personID, int Selection);
+    //Function that adds a person to the list
     void addToList(vector<TolPers> input);
+    //Function that sends a person to the recycle bin
     void deletePerson(int i);
+    //Function that empties the recycle bin
     void emptyTrash();
+    //Function that recovers everything from the recycle bin
     void recoverFromTrash(int i);
 private:
     vector<TolPers> _personur;
