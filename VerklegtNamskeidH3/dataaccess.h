d5c4f4ff8e8acd5fd056e515264ae8f5f22f3ff3
Eyþór Atli Sigurðsson <Eyr Atli Sigursson>
2016-12-02 16:44:52 +0000

    baetti vid sma kommentum

--- VerklegtNamskeidH3/dataaccess.h
+++ VerklegtNamskeidH3/dataaccess.h
@@ -5,8 +5,9 @@
 #include <fstream>
 
 using namespace std;
-
-struct TolPers{
+// Struct declared for the vectors we use.
+struct TolPers
+{
     string name;
     string middleInitial;
     string lastName;
@@ -14,17 +15,19 @@ struct TolPers{
     int yearOfBirth;
     int yearOfDeath;
 };
+
 class DataAccess
 {
 public:
     DataAccess();
-    void test();
+    // Function to retrive all info from both files that contain the list and the trash.
     void retriveInfo(vector<TolPers> &person, vector<TolPers> &tBin);
+    // Function that updates the file that contains the list.
     void updateFile(vector<TolPers> person);
+    // Function that updates the trash file.
     void updateTrash(vector<TolPers> trash);
+    // Function that deletes the trash file.
     void emptyTrash();
-private:
-
 };
 
 #endif // DATAACCESS_H
